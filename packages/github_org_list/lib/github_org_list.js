551b9a366cf66d84124a6f78d9f9f938a03febb6
Luca Mussi <ing.luca.mussi@gmail.com>
2014-12-28 09:30:28 +0100

    clean up and final improvements

--- packages/github_org_list/lib/github_org_list.js
+++ packages/github_org_list/lib/github_org_list.js
@@ -1,66 +1,131 @@
 'use strict';
 /* global githubOrgList: true */
 
+/**
+* githubOrgList module
+* provides both a 'githubOrgList' template to list a GitHub user's organizations
+* and a 'githubOrgList' object reactively providing the status of the template
+* @exports githubOrgList
+* @version 1.0
+*/
 
-var
-  ghOrgs = new Mongo.Collection("ghOrgs"),
-  sub,
-  ghOL = function(){}
-;
+// Local collection 'ghOrgs' for GitHub Organizations
+// It contains objects like this:
+//
+// {
+//   _id: 10003264
+//   avatar_url: 'https://avatars.githubusercontent.com/u/10003264?v=3'
+//   login: 'MeteorPackaging'
+//   name: 'meteorpackaging'
+//   type: 'org'
+// }
+var ghOrgs = new Mongo.Collection('ghOrgs');
 
+// Handle for the subscription to current user's organizations
+var sub;
+
+Tracker.autorun(function () {
+  // Trick to make this function reactive and
+  // be executed again on current user changes...
+  Meteor.user();
+
+  // Possibly stops a previsous subscription
+  // This is needed to get a fresh new subscription with false readiness satus
+  // when a user logs in. Otherwise the 'sub.ready()' seems to stay true...
+  if (sub) {
+    sub.stop();
+  }
+
+  // Subscribe to the list of GitHub organizations for the current user
+  sub = Meteor.subscribe('ghOrgs');
+});
+
+/**
+* githubOrgList @class
+* @constructor
+*/
+var ghOL = function(){};
+
+/**
+* Reactive object referencing the currently selected organization item.
+* @private @var
+*/
 ghOL.prototype._activeOrg = new ReactiveVar();
 
+/**
+* Array of rendered template instances.
+* @private @var
+*/
 ghOL.prototype._instances = [];
 
+/**
+* Clears the active organization.
+* @method
+*/
 ghOL.prototype.clearActiveOrg = function(){
   return this._activeOrg.set(undefined);
 };
 
+/**
+* Retrieves the active organization.
+* @method
+*/
 ghOL.prototype.getActiveOrg = function(){
   return this._activeOrg.get();
 };
 
+/**
+* Retrieves the list of rendered template instances.
+* @method
+*/
 ghOL.prototype.instances = function(){
   return this._instances;
 };
 
+/**
+* Sets the active organization.
+* @method
+* @param {object} orgObj - The organization to be set as the active one.
+* Returns true in case of successful active organization update.
+*/
 ghOL.prototype.setActiveOrg = function(orgObj){
   check(orgObj, Object);
+
+  var user = Meteor.user();
+
   // Checks if an organization with _id *id* exists
-  if (ghOrgs.findOne({_id: orgObj._id})) {
-    // Sets it as the active one
-    this._activeOrg.set(orgObj);
+  // or if the provided object represents the user
+  if (ghOrgs.findOne({_id: orgObj._id}) || (user && user._id === orgObj._id)) {
+    var activeOrg = this._activeOrg.get();
+
+    // Checks it is not already the current one
+    // to prevent useless reactive re-computations
+    if (!activeOrg || activeOrg._id !== orgObj._id) {
+      // Sets it as the active one
+      this._activeOrg.set(orgObj);
+    }
     return true;
   }
 
-  // Clears the active organization, since the required one does not exist
+  // Clears the active organization, in case the provided reference is invalid
   this._activeOrg.set(undefined);
   return false;
 };
 
-// Instantiation of the githubOrgList which exposes some current state
-// of the githubOrgList template
+// Instantiation of the githubOrgList which exposes
+// some current state of the githubOrgList template
+// to be used by other templates
 githubOrgList = new ghOL();
 
 
-Tracker.autorun(function () {
-  // Trick to make the call reactive tu current user changes...
-  Meteor.user();
-  // Possibly stops the previsous subscription
-  if (sub) {
-    sub.stop();
-  }
-  // Subscribe to the list of GitHub organizations
-  sub = Meteor.subscribe("ghOrgs");
-});
 
 Template.githubOrgList.rendered = function(){
-  // Adds the current instance to the list of template instances
+  // Adds the current instance to the array of template instances
   githubOrgList._instances.push(this);
 };
 
 Template.githubOrgList.destroyed = function(){
-  // Possibly removes the current instance from the list of template instances
+  // Possibly removes the current instance from the array of template instances
   var index = githubOrgList._instances.indexOf(this);
   if (index > -1) {
     githubOrgList._instances.splice(index, 1);
@@ -69,29 +134,59 @@ Template.githubOrgList.destroyed = function(){
 
 
 Template.githubOrgList.helpers({
+  user: function(){
+    /** Provides the object representing the current logged in user. */
+    var user = Meteor.user();
+    if (user) {
+      return {
+        _id: user._id,
+        avatar_url: user.profile.avatar_url,
+        login: user.profile.login,
+        name: user.profile.name,
+        type: 'user',
+      };
+    }
+  },
   orgs: function(){
-    // Returns organizations' name alphabetically sorted
-    return ghOrgs.find({}, {sort: {name: 1}});
+    // Returns organizations sorted by lower case 'login' (i.e. 'name')
+    // and adds a special `type` field to distinguish organization objects
+    // from the one about the current user
+    // returned objects are like this:
+    //
+    // {
+    //   _id: 10003264
+    //   avatar_url: 'https://avatars.githubusercontent.com/u/10003264?v=3'
+    //   login: 'MeteorPackaging'
+    //   name: 'meteorpackaging'
+    //   type: 'org'
+    // }
+    return ghOrgs.find({}, {sort: {name: 1}, transform: function(org){
+      org.type = 'org';
+      console.dir(org);
+      return org;
+    }});
   },
   active: function(){
-    // Returns 'active' in case this item corresponds to the active one
+    // Returns 'active' for the item corresponding to the selected organization
     var activeOrg = githubOrgList.getActiveOrg();
     if (activeOrg && activeOrg._id === this._id){
       return 'active';
     }
   },
   ready: function(){
-    // Trick to make the call reactive tu current user changes...
+    // Trick to make this function reactive and
+    // be executed again on current user changes...
     Meteor.user();
 
-    // Returns the readyness status of the subscription
+    // In case there's an open subscription
+    // returns its readyness status
     return sub && sub.ready();
   },
 });
 
 Template.githubOrgList.events({
   'click .item': function(){
-    // Changes the current active organization
+    // Changes the current active organization when a list item is clicked
     githubOrgList.setActiveOrg(this);
   },
 });
--- packages/github_org_list/lib/server.js
+++ packages/github_org_list/lib/server.js
@@ -3,21 +3,15 @@
 /*
 GitHub API Resources:
 
-https://developer.github.com/v3/repos/hooks/
-https://github.com/mikedeboer/node-github
+https://developer.github.com/v3/
 http://mikedeboer.github.io/node-github/
+https://github.com/mikedeboer/node-github
+https://github.com/mikedeboer/node-github/blob/master/api/v3.0.0/routes.json
 
 - Orgs
 
 https://developer.github.com/v3/orgs/
 http://mikedeboer.github.io/node-github/#orgs
-
-- Hooks
-
-http://mikedeboer.github.io/node-github/#repos.prototype.getHook
-http://mikedeboer.github.io/node-github/#repos.prototype.getHooks
-http://mikedeboer.github.io/node-github/#repos.prototype.createHook
-https://developer.github.com/v3/repos/hooks/#create-a-hook
 
 */
 
@@ -25,7 +19,7 @@ https://developer.github.com/v3/repos/hooks/#create-a-hook
 var GithubApi = Npm.require('github');
 
 var github = new GithubApi({
-  version: "3.0.0"
+  version: '3.0.0'
 });
 
 Meteor.publish('ghOrgs', function(){
@@ -34,35 +28,59 @@ Meteor.publish('ghOrgs', function(){
     user = Meteor.users.findOne(this.userId)
   ;
 
+  // Checks there's a logged in user
   if (user){
+    // Picks up username and access token from service data
     var
       ghs = user.services.github,
       token = ghs && ghs.accessToken,
       username = ghs && ghs.username
     ;
 
+    // Provides authorization details for the next github call
     if (token) {
       github.authenticate({
-        type: "oauth",
+        type: 'oauth',
         token: token
       });
     }
 
+    // Gets the list of organization for the current user
     github.orgs.getFromUser({
       user: username
     }, function(err, data) {
       if (!err) {
+        // In case of successful return provides a new document
+        // into the 'fake' ghOrgs collection for each found organization
         _.each(data, function(org){
-          self.added("ghOrgs", org.id, {
-            name: org.login,
-            avatarUrl: org.avatar_url,
-          });
+          var orgId = org.id;
+
+          // Sanity check: not all objects actually represent an organization
+          if (orgId) {
+
+            // Keeps only 'login' (the organization name) and 'avatar_url'
+            var orgObj = _.pick(org, [
+              'login',
+              'avatar_url',
+            ]);
+
+            // Adds a lower cased name just for sorting purposes
+            orgObj.name = orgObj.login.toLowerCase();
+
+            // Signals the new document
+            self.added('ghOrgs', orgId, orgObj);
+          }
         });
       }
+
+      // When all organizations have been processed,
+      // sets the subscription as ready
       self.ready();
     });
   }
   else{
+    // In case there's no user logged in,
+    // sets the subscription as ready straight on
     self.ready();
   }
 });
