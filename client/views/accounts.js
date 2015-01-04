cd21500d77540ff4a50e64f31f743030fac7cf99
Luca Mussi <ing.luca.mussi@gmail.com>
2015-01-04 17:45:05 +0100

    new page for orgs/repos list

--- client/views/accounts.js
+++ client/views/accounts.js
@@ -3,13 +3,7 @@
 
 // Reactively Links org list and repo list
 Tracker.autorun(function () {
-  var activeOrg = githubOrgList.getActiveOrg();
-  if (activeOrg){
-    activeOrg = _.pick(activeOrg, ['login', 'type']);
-  }
-
-  githubRepoList.setSelectedOrg(activeOrg);
+  githubRepoList.setSelectedOrg(githubOrgList.getActiveOrg());
 });
-
 
 githubRepoList.floatTmpl = 'repoInfo';
