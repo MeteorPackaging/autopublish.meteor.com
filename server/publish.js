e97ff5adeca9db176dde7f9b10d6b638c6d51102
Luca Mussi <ing.luca.mussi@gmail.com>
2015-01-04 17:42:34 +0100

    stub publications

--- server/publish.js
+++ server/publish.js
@@ -1,9 +1,9 @@
 'use strict';
-/* global AutoPublish: false, Meteor: false */
+/* global AutoPublish: false, Meteor: false, Subscriptions: false */
 
 Meteor.publish('queueingPublish', function(limit){
 	check(limit, Number);
-
+	Meteor._sleepForMs(3000);
 	return AutoPublish.find({
 		completedAt: { $exists: false }
 	}, {
@@ -17,6 +17,14 @@ Meteor.publish('completedPublish', function(limit){
 	return AutoPublish.find({
 		completedAt: { $exists: true }
 	}, {
+		limit: limit
+	});
+});
+
+Meteor.publish('subscriptions', function(limit){
+	check(limit, Number);
+
+	return Subscriptions.find({}, {
 		limit: limit
 	});
 });
