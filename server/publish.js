98ed60b9e40f9c87645a55e56c744807e06a006a
Luca Mussi <ing.luca.mussi@gmail.com>
2015-01-05 00:47:56 +0100

    sorted publish

--- server/publish.js
+++ server/publish.js
@@ -1,30 +1,30 @@
 'use strict';
-/* global AutoPublish: false, Meteor: false, Subscriptions: false */
+
+/* global
+    AutoPublish: false,
+    completedSelector: false,
+    newestCompleted: false,
+    oldestQueueing: false,
+    queueingSelector: false,
+    Subscriptions: false
+*/
 
 Meteor.publish('queueingPublish', function(limit){
-	check(limit, Number);
-	Meteor._sleepForMs(3000);
-	return AutoPublish.find({
-		completedAt: { $exists: false }
-	}, {
-		limit: limit
-	});
+  check(limit, Number);
+  return AutoPublish.find(queueingSelector,
+    _.extend({limit: limit}, oldestQueueing)
+  );
 });
 
 Meteor.publish('completedPublish', function(limit){
-	check(limit, Number);
-
-	return AutoPublish.find({
-		completedAt: { $exists: true }
-	}, {
-		limit: limit
-	});
+  check(limit, Number);
+  return AutoPublish.find(
+    completedSelector,
+    _.extend({limit: limit}, newestCompleted)
+  );
 });
 
 Meteor.publish('subscriptions', function(limit){
-	check(limit, Number);
-
-	return Subscriptions.find({}, {
-		limit: limit
-	});
+  check(limit, Number);
+  return Subscriptions.find({}, {limit: limit});
 });
