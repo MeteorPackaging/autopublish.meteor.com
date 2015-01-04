fb81c2a7d593bbac58854f7cb25fa09d5ec99e9f
Luca Mussi <ing.luca.mussi@gmail.com>
2015-01-05 00:43:12 +0100

    column order, and new selectors

--- client/views/home.js
+++ client/views/home.js
@@ -1,20 +1,27 @@
 'use strict';
 
-/* global AutoPublish: false, Subscriptions: false */
+/* global
+    AutoPublish: false,
+    completedSelector: false,
+    newestCompleted: false,
+    oldestQueueing: false,
+    queueingSelector: false,
+    Subscriptions: false
+*/
 
 Template.home.helpers({
+  // Alphabetically sorted
+  alpha: {pkgName: 1},
+  // Returns the AutoPublish collection
   AutoPublish: AutoPublish,
+  // Returns the selector to be used to retrieve completed publish operations
+  completedSelector: completedSelector,
+  // Newest completed first
+  newest: newestCompleted,
+  // Oldest queueing first
+  oldest: oldestQueueing,
+  // Returns the Subscriptions collection
   Subscriptions: Subscriptions,
-  queueing: function(){
-    // Returns the selector to be used to retrieve queueing publish operations
-    return {
-      completedAt: { $exists: false }
-    };
-  },
-  completed: function(){
-    // Returns the selector to be used to retrieve completed publish operations
-    return {
-      completedAt: { $exists: true }
-    };
-  },
+  // Returns the selector to be used to retrieve queueing publish operations
+  queueingSelector: queueingSelector,
 });
