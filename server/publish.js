1e2899bff92be89014426f6e065b19a1bfcac513
Luca Mussi <ing.luca.mussi@gmail.com>
2015-01-11 08:50:03 +0100

    leaner publications

--- server/publish.js
+++ server/publish.js
@@ -12,7 +12,19 @@
 Meteor.publish('queueingPublish', function(limit){
   check(limit, Number);
   return AutoPublish.find(queueingSelector,
-    _.extend({limit: limit}, oldestQueueing)
+    _.extend(
+      {limit: limit},
+      oldestQueueing,
+      {
+        fields: {
+          createdAt: 0,
+          log: 0,
+          releaseTargetCommittish: 0,
+          repoCloneUrl: 0,
+          version: 0,
+        }
+      }
+    )
   );
 });
 
@@ -20,11 +32,33 @@ Meteor.publish('completedPublish', function(limit){
   check(limit, Number);
   return AutoPublish.find(
     completedSelector,
-    _.extend({limit: limit}, newestCompleted)
+    _.extend(
+      {limit: limit},
+      newestCompleted,
+      {
+        fields: {
+          createdAt: 0,
+          log: 0,
+          releaseTargetCommittish: 0,
+          repoCloneUrl: 0,
+          tagName: 0,
+        }
+      }
+    )
   );
 });
 
 Meteor.publish('subscriptions', function(limit){
   check(limit, Number);
-  return Subscriptions.find({}, {limit: limit});
+  return Subscriptions.find({}, {
+    limit: limit,
+    fields: {
+      hookEvents: 0,
+      hookId: 0,
+      repo: 0,
+      repoId: 0,
+      tested: 0,
+      user: 0
+    }
+  });
 });
