c25a547d5c59d5c17ea27226a928181ba6e38690
Luca Mussi <ing.luca.mussi@gmail.com>
2015-01-11 16:02:59 +0100

    switched to ss2shell

--- server/meteor.js
+++ server/meteor.js
@@ -1,267 +1,361 @@
-/* global publishPackage: true */
+/* global publishPackage: true, AutoPublish: false */
 
 // General meteor integration
 
-var sshConnect = Meteor.npmRequire('ssh2-connect');
+var sshShell = Meteor.npmRequire('ssh2shell');
 
-var machine = function() {
+//Host Object:
+var host = function(serverDetails, msgCallback){
   'use strict';
 
-  var execHandlers = [];
-  var progressHandlers = [];
-  var doneCallback;
+  this.server = serverDetails;
+  this.commands = [];
+  this.msg = msgCallback;
 
-  var errorCallback = function(err) {
-    // Exit now
-    doneCallback(err);
-    ssh.end();
-  };
+  // Standard messages
+  this.connectedMessage = "Connected to Build Machine";
+  this.readyMessage = "Now Running publish sequence";
+  this.closedMessage = "Completed";
 
-  var progressCallback = function(index, cmd) {
-    progressHandlers.forEach(function(handler) {
-      try {
-        handler(index, cmd);
-      } catch(err) {
-        console.error('Progress callback for machine failed!');
-        throw err;
-      }
-    });
-  };
+  this.commandProcessingActions = [];
+  this.commandCompleteActions = [];
+  this.endActions = [];
 
-  var runCommand = function(ssh, command, callback) {
-    ssh.exec(command.cmd, command.options, function(err, stream) {
-      var
-        result = '',
-        stderr = ''
-      ;
+  // Final result object where command actions can add stuff...
+  this.result = {};
+  // Possible error list where command actions can log errors...
+  this.errors = [];
 
-      if (err) {
-        callback(err);
-      } else {
-        stream.on('data', function(data) {
-          //console.log('STDOUT: ' + data);
-          result += data.toString();
-        });
+  // Debug options
+  //this.verbose = true;
+  //this.debug = true;
+};
 
-        stream.stderr.on('data', function(data) {
-          //console.log('STDERR: ' + data);
-          stderr += data.toString();
-        });
-
-        stream.on('error', function(err) {
-          //console.log('error: ' + err);
-          callback(err, result, stderr);
-        });
-
-        stream.on('end', function() {
-          // Removes the trailing newline...
-          result = result.replace(/\n$/, '');
-          stderr = stderr.replace(/\n$/, '');
-          callback(null, result, stderr);
-        });
-      }
-    });
-  };
-
-  // ssh.end();
-  var start = function() {
-    sshConnect({
-      host: Meteor.settings.host,
-      username: Meteor.settings.username,
-      privateKey: Meteor.settings.key
-    }, function(err, ssh){
-      if (err) {
-        doneCallback(err);
-      } else {
-        var next = function() {
-          // Get the first command
-          var command = execHandlers.shift();
-          // Check to make sure we are not done
-          if (command) {
-            // Update progress
-            progressCallback(execHandlers.length, command.cmd);
-            // Run the command
-            runCommand(ssh, command, function(err, result, stderr) {
-              if (err) {
-                // Run error callbacks
-                errorCallback(err);
-              } else {
-                try {
-                  // Run command callback
-                  command.f(result, stderr);
-                  // Next command
-                  next();
-                } catch(err) {
-                  doneCallback(err);
-                  ssh.end();
-                }
-              }
-            });
-          } else {
-            // End this connection
-            doneCallback();
-            ssh.end();
-          }
-        };
-        // Initial start
-        next();
-      }
-    });
-  }; // EO start
-
-  var element = {
-    exec: function(cmd, options, callback) {
-      // Just check arguments before pushing it to task list
-      if (arguments.length === 2) {
-        if (typeof options !== 'function') {
-          throw new Error('exec expects callback function');
-        }
-        execHandlers.push({
-          cmd: cmd,
-          options: {},
-          f: options
-        });
-      } else if (arguments.length === 3) {
-        if (typeof callback !== 'function') {
-          throw new Error('exec expects callback function');
-        }
-        execHandlers.push({
-          cmd: cmd,
-          options: options,
-          f: callback
-        });
-      } else {
-        throw new Error('exec expects (cmd, [options], callback)');
-      }
-      return element;
-    },
-    progress: function(f) {
-      if (typeof f !== 'function') {
-        throw new Error('progress expects callback function');
-      }
-      progressHandlers.push(f);
-      return element;
-    },
-    done: function(f) {
-      if (typeof f !== 'undefined' && typeof f !== 'function') {
-        throw new Error('done expects callback function');
-      }
-      doneCallback = f;
-      // Start connection
-      start();
+host.prototype.onCommandProcessing = function(cmd, response, hostObj, stream) {
+  'use strict';
+  var self = this;
+  _.each(this.commandProcessingActions, function(action){
+    if (cmd === action.cmd){
+      action.callback.call(self, response, hostObj, stream);
     }
-  };
+  });
+};
 
-  return element;
+host.prototype.onCommandComplete = function(cmd, response, hostObj) {
+  //response is the full response from the command completed
+  //hostObj is this object and gives access to the current set of commands
+  'use strict';
+
+  // XXX: check the "\r\n" line break on different OS!
+  var responses = response.split("\r\n");
+
+  // Possibly saves the prompt text for later removal
+  if (!this.prompt && responses.length > 0) {
+    this.prompt = responses[responses.length - 1];
+  }
+  // Possibly removes the first command line
+  if (cmd.length > 0) {
+    responses = responses.slice(1);
+  }
+  // Removes the last prompt line
+  responses = responses.slice(0, -1);
+
+  // Back to single response string
+  response = responses.join("\n");
+
+  var self = this;
+  _.each(this.commandCompleteActions, function(action){
+    if (cmd === action.cmd){
+      action.callback.call(self, response, hostObj);
+    }
+  });
+};
+
+host.prototype.onEnd = function(sessionText, hostObj) {
+  //sessionText is the full text for this hosts session
+
+  'use strict';
+
+  // Retrieves the prompt string
+  var prmpt = hostObj.prompt;
+  prmpt = prmpt.split('~');
+  // Escapes special characters
+  prmpt[0] = prmpt[0].replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
+  prmpt[1] = prmpt[1].replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
+  // Accepts everything from the initial prompt up to the $
+  prmpt = prmpt[0] + '.*' + prmpt[1];
+  // Removes the prompt from sessionText
+  var prmptRe = new RegExp(prmpt, "mg");
+  sessionText = sessionText.replace(prmptRe, "");
+  // Removes "Connected to xxx.xxx.xxx.xxx" from sessionText
+  sessionText = sessionText.replace(/Connected to [0-9.\n\r]*/g, "");
+
+  // Removes trailing new line from sessionText
+  sessionText = sessionText.replace(/\n$/, '');
+
+  var self = this;
+  _.each(this.endActions, function(action){
+    action.call(self, null, sessionText, hostObj);
+  });
+};
+
+host.prototype.addCommand = function(cmd){
+  'use strict';
+  this.commands.push(cmd);
+};
+
+host.prototype.addCommandToFront = function(cmd){
+  'use strict';
+  this.commands.unshift(cmd);
+};
+
+host.prototype.addProcessingAction = function(action){
+  'use strict';
+  this.commandProcessingActions.push(action);
+};
+
+host.prototype.addCompleteAction = function(action){
+  'use strict';
+  this.commandCompleteActions.push(action);
+};
+
+host.prototype.addEndAction = function(action){
+  'use strict';
+  this.endActions.push(action);
+};
+
+host.prototype.endCommands = function(){
+  'use strict';
+  while(this.commands.length > 0) {
+    this.commands.pop();
+  }
+  this.commands.push("exit");
+};
+
+host.prototype.run = function(endCallback){
+  'use strict';
+  this.addCommand("exit");
+  //Create a new instance
+  var ssh = new sshShell(this);
+  var self = this;
+
+  if (endCallback) {
+    self.addEndAction(endCallback);
+
+    ssh.on("error", function onError(err, type, close, callback) {
+      endCallback(err, self.sessionText, self);
+    });
+  }
+
+  //Start the process
+  this.connection = ssh.connect();
+
+  return ssh;
+};
+
+var clearGitSessionText = function(sessionText){
+  'use strict';
+  // Removes useless git clone output lines...
+  sessionText = sessionText.replace(/^remote: Compressing.*\r/gm, '');
+  sessionText = sessionText.replace(/^Receiving objects.*\r/mg, '');
+  sessionText = sessionText.replace(/^Resolving deltas.*\r/mg, '');
+  return sessionText;
+};
+
+var clearMeteorSessionText = function(sessionText){
+  'use strict';
+  // Removes useless meteor publish output lines...
+  sessionText = sessionText.replace(/^   Preparing project.*\r/gm, '');
+  sessionText = sessionText.replace(/^   Updating package catalog.*\r/gm, '');
+  sessionText = sessionText.replace(/^   Initializing packages.*\r/gm, '');
+  sessionText = sessionText.replace(/^   Selecting package versions.*\r/gm, '');
+  sessionText = sessionText.replace(/^   Downloading missing packages.*\r/gm, '');
+  sessionText = sessionText.replace(/^   Building local packages.*\r/gm, '');
+  sessionText = sessionText.replace(/^   Preparing to build package.*\r/gm, '');
+  sessionText = sessionText.replace(/^   Building package.*\r/gm, '');
+  sessionText = sessionText.replace(/^   Publishing the package.*\r/gm, '');
+  sessionText = sessionText.replace(/^   Bundling source.*\r/gm, '');
+  sessionText = sessionText.replace(/^   Creating package version.*\r/gm, '');
+  sessionText = sessionText.replace(/^   Uploading source.*\r/gm, '');
+  sessionText = sessionText.replace(/^   Publishing package version.*\r/gm, '');
+  sessionText = sessionText.replace(/^   Bundling build.*\r/gm, '');
+  sessionText = sessionText.replace(/^   Creating package build.*\r/gm, '');
+  sessionText = sessionText.replace(/^   Uploading build.*\r/gm, '');
+  sessionText = sessionText.replace(/^   Publishing package build.*\r/gm, '');
+  sessionText = sessionText.replace(/^   Creating package.*\r/gm, '');
+  sessionText = sessionText.replace(/^\s*[\\\|\/\-]\r/gm, '');
+  sessionText = sessionText.replace(/^\s*\r/gm, '');
+  return sessionText;
 };
 
 publishPackage = function(pkgInfo, callback) {
   'use strict';
 
-  var
-    result = {
-      log: [],
-      errors: [],
-    },
-    gitClone =
-      "git clone -b "+
-      pkgInfo.tagName +
-      " " +
-      pkgInfo.repoCloneUrl +
-      " autopublish"
+  var progress = {
+    send: Meteor.bindEnvironment(function(msg){
+      // Possibly removes initial IP address of the build machie
+      msg = msg.replace(/^[0-9.]*: /, "");
+
+      // Updates the publish action object to show this last message
+      AutoPublish.update(pkgInfo._id, {
+        $set: {
+          publishing: msg
+        }
+      });
+    })
+  };
+
+  // Creates the ssh interface to the remote machine
+  // XXX: this should be transformed into a cycle to get machines
+  //      for all three architectures when it is the case.
+  var machine = new host(
+    Meteor.settings.machine['os.linux.x86_64'],
+    progress
+  );
+
+  // Commands
+  var cmd;
+
+  // Checks for currently installed Meteor Version
+  cmd = '~/.meteor/meteor --version';
+  machine.addCommand('msg:Checking Meteor Version...');
+  machine.addCommand(cmd);
+  machine.addCompleteAction({
+    cmd: cmd,
+    callback: function(version, hostObj) {
+      if (version) {
+        hostObj.result.meteorVersion = version;
+      } else {
+        hostObj.errors.push('Could not run meteor on remote machine');
+        this.endCommands();
+      }
+    }
+  });
+
+  // Checks currently logged in meteor user
+  cmd = '~/.meteor/meteor whoami';
+  machine.addCommand('msg:Checking Meteor User...');
+  machine.addCommand(cmd);
+  machine.addCompleteAction({
+    cmd: cmd,
+    callback: function(username, hostObj) {
+      if (username) {
+        hostObj.result.username = username;
+      } else {
+        hostObj.result.success = false;
+        hostObj.errors.push('Could not login user on remote machine');
+        this.endCommands();
+      }
+    }
+  });
+
+  // Possibly removes old autopublish folder
+  cmd = 'rm -rf autopublish';
+  machine.addCommand('msg:Preparing environment...');
+  machine.addCommand(cmd);
+
+  // Clones the repository
+  cmd = "git clone -b " + pkgInfo.tagName + " " + pkgInfo.repoCloneUrl +
+    " autopublish"
   ;
+  machine.addCommand('msg:Cloning Repository...');
+  machine.addCommand(cmd);
 
-  machine()
-  // Grap version
-  .exec('~/.meteor/meteor --version', function(version) {
-    if (version) {
-      result.meteorVersion = version;
-    } else {
-      throw new Error('Could not run remote meteor');
+  // Moves into the cloned repo
+  cmd = 'cd autopublish';
+  machine.addCommand(cmd);
+  machine.addCompleteAction({
+    cmd: cmd,
+    callback: function(response, hostObj) {
+      if (/No such file or directory/.test(response)){
+        hostObj.result.success = false;
+        hostObj.errors.push('Unable to clone the repository');
+        this.endCommands();
+      }
+      else {
+        this.addCommandToFront('msg:' +
+          'Running publish ' +
+          pkgInfo.pkgName +
+          ' on ' +
+          hostObj.result.meteorVersion +
+          '...'
+        );
+      }
     }
-  })
- .exec('~/.meteor/meteor whoami', function(username) {
-    if (username && username.length) {
-      result.username = username;
-    } else {
-      throw new Error('Could not login user on remote');
+  });
+
+  // Runs 'meteor publish'
+  cmd = "~/.meteor/meteor publish";
+  var afterPublishCallback = function(response, hostObj) {
+    response = clearMeteorSessionText(response);
+    if (/Errors while publishing/.test(response)){
+      // Some error occurred...
+      if (/Version already exists/.test(response)) {
+        // Not published, version already existed!
+        hostObj.result.success = false;
+        hostObj.errors.push("Version already exists");
+        this.endCommands();
+      } else if (/There is no package named/.test(response)) {
+        // Not published, first time publishing!
+        // Triggers a `meteor publish --create` command
+        var cmd = "~/.meteor/meteor publish --create";
+        machine.addCommandToFront(cmd);
+        machine.addCommandToFront("msg:First time publishing...");
+        machine.addCompleteAction({
+          cmd: cmd,
+          callback: afterPublishCallback
+        });
+      }
+      else {
+        hostObj.result.success = false;
+        hostObj.errors.push("Some error occurred");
+        this.endCommands();
+      }
     }
-  })
-  .exec('pwd && rm -rf autopublish', function(data) {
-    if (data.length) {
-      result.log.push(data);
-    }
-    console.log("- Cloning repository...");
-  })
-  .exec(gitClone, function(data) {
-    if (data.length) {
-      result.log.push(data);
-    }
-    console.log(
-      '- Running publish ' +
-      pkgInfo.pkgName +
-      ' on Meteor@' +
-      result.meteorVersion +
-      '...(please wait)'
-    );
-  })
-  .exec('cd autopublish && ~/.meteor/meteor publish', function(data, stderr) {
-    // XXX: Shold prop catch errors?
-    // data = 'Published packaging:autopublish-test@0.0.9.' after success
-    // stderr = '=> Errors while publishing:\n\nWhile creating package version:\nerror: Version already exists'
-    // after unsuccessful publish
-
-
-    /*
-    => Errors while publishing:
-
-    While publishing the package:
-    error: There is no package named packaging:autopublish-fake-test. If you are
-    creating a new package, use the --create flag.
-    */
-
-
-    if (/Published/.test(data)) {
+    // Otherwise extracts
+    else if (/Published/.test(response)) {
       // Successfully published!
-      var ver = data.split('@');
+      hostObj.result.success = true;
+
+      // Gets the published version
+      var ver = response.split('@');
+      hostObj.result.version = ver[1].replace(/.$/, '');
+    }
+    else {
+      hostObj.result.success = false;
+      hostObj.errors.push("Some error occurred");
+      this.endCommands();
+    }
+  };
+  machine.addCommand(cmd);
+  machine.addCompleteAction({
+    cmd: cmd,
+    callback: afterPublishCallback
+  });
+
+  // Everything done!
+  machine.addCommand('msg:Done!');
+
+
+  // Actually starts the command sequence
+  machine.run(function(err, sessionText, hostObj){
+    // Possibly cleans up the git output
+    sessionText = clearGitSessionText(sessionText);
+    // Possibly cleans up the meteor publish output
+    sessionText = clearMeteorSessionText(sessionText);
+
+    var result = hostObj.result;
+    // Stores the sessionText as the sequence log
+    result.log = sessionText;
+
+    if (hostObj.errors.length > 0){
+      result.errors = hostObj.errors;
+    }
+    else {
       result.success = true;
-      result.version = ver[1].replace(/.$/, '');
-    } else if (/Version already exists/.test(stderr)) {
-      // Not published, version already existed!
-      result.success = false;
-      result.errors.push("Version already exists");
-    } else if (/There is no package named/.test(stderr)) {
-      // Not published, first time publishing!
-      // XXX: should trigger a `meteor publish --create` command
-    } else if (!data && stderr) {
-      // Not published, some other error!
-      result.success = false;
-      result.errors.push(stderr);
     }
 
-    console.log("After Meteor Publish:");
-    console.log("data:");
-    console.log(data);
-    console.log("stderr:");
-    console.log(stderr);
-
-    if (data.length) {
-      result.log.push(data);
-    }
-  })
-  .exec('pwd && rm -rf autopublish', function() {
-    console.log("Remote work done!");
-  })
-  // .progress(function(index, cmd) {
-  //   console.log('Progress:', index, cmd);
-  // })
-  .done(function(err) {
-    if (!result.log.length){
-      delete result.log;
-    }
-    if (!result.errors.length){
-      delete result.errors;
-    }
-    callback(err, result);
+    // Eventually calls the final callback
+    callback(null, result);
   });
 };
