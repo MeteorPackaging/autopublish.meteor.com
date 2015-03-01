78c4eec98a33f0f7ba05b5829985f07782259543
Luca Mussi <ing.luca.mussi@gmail.com>
2015-03-01 21:28:12 +0100

    known hooks migration

---
+++ server/migrations/known_hooks.js
@@ -0,0 +1,16 @@
+/* global
+    HookPayloads: false,
+    processHookPingEvent: false
+*/
+'use strict';
+
+
+Meteor.startup(function(){
+  // Create KnownHooks documents from ping events received in the past
+  HookPayloads.find().forEach(function(doc){
+    var payload = doc.payload;
+    if (payload.zen) {
+      processHookPingEvent(payload);
+    }
+  });
+});
